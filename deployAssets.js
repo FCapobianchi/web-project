7f9ac50072e531abb38bf458a9841cb255b0ee9b
fabiano.capobianchi <fabiano.capobianchi@agid.gov.it>
2022-07-15 19:14:24 +0200

    Aggiunto daterange picker

--- deployAssets.js
+++ deployAssets.js
@@ -117,3 +117,21 @@ cpr('node_modules/bootstrap/dist/', 'www/assets/bootstrap', {
   //err - The error if any (err.list might be available with an array of errors for more detailed information)
   //files - List of files that we copied
 });
+
+cpr('node_modules/daterangepicker/daterangepicker.js', 'www/assets/daterangepicker/daterangepicker.js', {
+  deleteFirst: true, //Delete "to" before
+  overwrite: true, //If the file exists, overwrite it
+  confirm: true //After the copy, stat all the copied files to make sure they are there
+}, function(err, files) {
+  //err - The error if any (err.list might be available with an array of errors for more detailed information)
+  //files - List of files that we copied
+});
+
+cpr('node_modules/daterangepicker/daterangepicker.css', 'www/assets/daterangepicker/daterangepicker.css', {
+  deleteFirst: true, //Delete "to" before
+  overwrite: true, //If the file exists, overwrite it
+  confirm: true //After the copy, stat all the copied files to make sure they are there
+}, function(err, files) {
+  //err - The error if any (err.list might be available with an array of errors for more detailed information)
+  //files - List of files that we copied
+});
