cbeffc55493728907fe38160a266df3978d906b0
fabiano.capobianchi <fabiano.capobianchi@agid.gov.it>
2022-07-15 17:34:39 +0200

    Avanzamanto lavori

--- deployAssets.js
+++ deployAssets.js
@@ -108,3 +108,12 @@ cpr('node_modules/nette.ajax.js/extensions', 'www/assets/nette/extensions', {
   //err - The error if any (err.list might be available with an array of errors for more detailed information)
   //files - List of files that we copied
 });
+
+cpr('node_modules/bootstrap/dist/', 'www/assets/bootstrap', {
+  deleteFirst: true, //Delete "to" before
+  overwrite: true, //If the file exists, overwrite it
+  confirm: true //After the copy, stat all the copied files to make sure they are there
+}, function(err, files) {
+  //err - The error if any (err.list might be available with an array of errors for more detailed information)
+  //files - List of files that we copied
+});
